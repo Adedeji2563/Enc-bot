14e60e0e7ba6862cf4f97d21f98b25cf688ac3c5
Adedeji2563 <adedejiadesida1@gmail.com>
2025-04-10 00:15:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  TELEGRAM_BOT_TOKEN: process.env.BOT_TOKEN,
+  TELEGRAM_BOT_TOKEN: process.env.BOT_TOKEN ||'8018532058:AAExGZVU2VNaKX_53xPw7Ps8B82pqAf6zDc',
   SECRET_KEY: process.env.SECRET_KEY || 'Shadow',
   ADMIN_ID: process.env.ADMIN_ID || '7092629860', // Your Telegram ID
   BANNER_IMAGE: 'https://files.catbox.moe/ljnxao.png'
